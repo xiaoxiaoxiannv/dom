ca1ab6f5e79bbd4475ed1f5e96cdab82d7b33e87
Luna <1017929760@qq.com>
2020-11-11 16:23:59 +0800

    templateè可放任何元素â

---
+++ src/dom.js
@@ -0,0 +1,8 @@
+window.dom = {
+    create(string){
+        const container = document.createElement('template');
+        container.innerHTML = string.trim();
+        return container.content.firstChild;
+    }
+};
+
---
+++ src/main.js
@@ -0,0 +1,2 @@
+const div = dom.create("<td>1</td>");
+console.log(div);
